882e200a0c9a440c28ea423ba92d38fdad3dd50f
Rodrigo De Castro Soares (THP) <RO20119661@wipro.com>
2019-10-14 14:34:02 -0300

    spring batch example

---
+++ cni-batch/src/main/java/br/com/cni/batch/cnibatch/CniBatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.cni.batch.cnibatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CniBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CniBatchApplication.class, args);
+	}
+
+}
